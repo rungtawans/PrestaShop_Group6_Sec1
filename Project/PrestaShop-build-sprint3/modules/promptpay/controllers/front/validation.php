c836a6f69570a84320d09a9e7f82cb1037cfb15d
Rungtawan Suttho <rungtawan.s@kkumail.com>
2024-02-25 19:55:08 +0700

    new database backup

--- Project/PrestaShop-build-sprint3/modules/promptpay/controllers/front/validation.php
+++ Project/PrestaShop-build-sprint3/modules/promptpay/controllers/front/validation.php
@@ -14,34 +14,38 @@ class PromptPayValidationModuleFrontController extends ModuleFrontController
         $cart = $context->cart;
 
         if (Tools::isSubmit('submitPaymentSlip')) {
+            
             if (isset($_FILES['payment_slip']) && !empty($_FILES['payment_slip']['name'])) {
-                // Validate the uploaded file (e.g., file type, size, etc.)
+                // Tools::redirect('index.php');
+
+            //     // Validate the uploaded file (e.g., file type, size, etc.)
                 $validFile = $this->validateFile($_FILES['payment_slip']);
 
-                if ($validFile) {
-                    // Process the payment details and update order status
-                    $this->module->validateOrder(
-                        $cart->id, Configuration::get('PS_OS_PAYMENT'), $cart->getOrderTotal(),
-                        $this->module->displayName, null, array(), null, false, $cart->secure_key
-                    );
-                    $customer = new Customer($cart->id_customer);
-                    if (!Validate::isLoadedObject($customer)) {
-                    Tools::redirect('index.php?controller=order&step=1');
-                    }
-                    Tools::redirect('index.php?controller=order-confirmation&id_cart=' 
-                        . $cart->id . '&id_module=' . $this->module->id . '&id_order=' 
-                        . $this->module->currentOrder . '&key=' . $customer->secure_key);
-                    return;
-                } else {
-                    $this->errors[] = $this->module->l('Invalid file. Please upload a valid payment slip.');
-                }
-            } else {
-                $this->errors[] = $this->module->l('Please upload a payment slip.');
+            //     if ($validFile) {
+            //         // Process the payment details and update order status
+            //         // $this->module->validateOrder(
+            //         //     $cart->id, Configuration::get('PS_OS_PAYMENT'), $cart->getOrderTotal(),
+            //         //     $this->module->displayName, null, array(), null, false, $cart->secure_key
+            //         // );
+            //         // $customer = new Customer($cart->id_customer);
+            //         // if (!Validate::isLoadedObject($customer)) {
+            //         // Tools::redirect('index.php?controller=order&step=1');
+            //         // }
+            //         // Tools::redirect('index.php?controller=order-confirmation&id_cart=' 
+            //         //     . $cart->id . '&id_module=' . $this->module->id . '&id_order=' 
+            //         //     . $this->module->currentOrder . '&key=' . $customer->secure_key);
+            //         Tools::redirect('index.php');
+            //         return;
+            //     } else {
+            //         $this->errors[] = $this->module->l('Invalid file. Please upload a valid payment slip.');
+            //     }
+            // } else {
+            //     $this->errors[] = $this->module->l('Please upload a payment slip.');
             }
         }
 
         // Set the template for displaying errors
-        $this->setTemplate('module:promptpay/views/templates/front/payment_form.tpl');
+        // $this->setTemplate('module:promptpay/views/templates/front/payment_form.tpl');
     }
 
     private function validateFile($file)
