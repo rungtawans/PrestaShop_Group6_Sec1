1e95c65c3cb0b452ec4b5726435bcf6d1029e595
Rungtawan Suttho <rungtawan.s@kkumail.com>
2024-02-27 02:52:34 +0700

    add new project floder

---
+++ Project/PrestaShop-sprint3-new/html/var/cache/prod/appParameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'admin',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'usAGC52KNa1k0kUwxHf9NehSZEIChVkjVHnFxUV6U8tkNkVeDhurRMXyZK5A5bby',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-02-27',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'O4HkEDaCdk5F2RHt6PYhuzPzmb5ThWTAJdqdrkkOPqWSZgoyKCUhqEE2ftsgoap0',
+    'cookie_iv' => 'FEs9Y2wUGqwnufFjnjPA99JOVOaef8eE',
+    'new_cookie_key' => 'def0000016a4da6cd5eb2d901215af698ff45e6ca841e702d550ee3d6fcdf7f45df3d8eb1ead8c3ab3bd3b8fd6e94032a25c0285465c2207b62eb6d6dc1e1501bac9010f',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiyjMAWCRJdkiQCZPrIYx
+jle4MhyK1AKlG+Ho3FmV3ntwAC+GhKv/ANKksUWCN0SD9uXuydePXb5T0klEDJEb
+O115ShIrixxo/mdYa4I61HgWtgEEUkZwdWbRScUO/THfE4WIze4D/OIJ2mMTW8yu
+EEdRyoOGG8d29K+MfgWSSHm5Ln1WT/W/ewX4ZnO3wLnxezuJMixQ/bMOEj5sYJ7i
+qttMKsmKr4+QTVqY7Iu8q9DsDsYdiiUubRUTw+shs2qEwWyVpGyV717ZqQ+ANn4Y
+K+ZKFhN4s1Ot7/VRqTBDHmd33D7YML42O5UVoPMFejmx33qoAkGDKn0JGJZPcDM6
+AwIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCLKMwBYJEl2SJA
+Jk+shjGOV7gyHIrUAqUb4ejcWZXee3AAL4aEq/8A0qSxRYI3RIP25e7J149dvlPS
+SUQMkRs7XXlKEiuLHGj+Z1hrgjrUeBa2AQRSRnB1ZtFJxQ79Md8ThYjN7gP84gna
+YxNbzK4QR1HKg4Ybx3b0r4x+BZJIebkufVZP9b97Bfhmc7fAufF7O4kyLFD9sw4S
+PmxgnuKq20wqyYqvj5BNWpjsi7yr0OwOxh2KJS5tFRPD6yGzaoTBbJWkbJXvXtmp
+D4A2fhgr5koWE3izU63v9VGpMEMeZ3fcPtgwvjY7lRWg8wV6ObHfeqgCQYMqfQkY
+lk9wMzoDAgMBAAECggEAHjLWQp5AIWSDaERoGdptOqSSll1XSzbfubNGozrVqRhE
+b+XgXT4t8nHDx6x7256EvLmdHjQnKoK5EfNQTyHWNQXN4mMbC+84YazvpTT3q0/8
+Lbb+xxP+J5wOYuHepNeDv2rFC1tRhjlPRhMmCk/anjG78CSmn7SjfenypQ50dkGS
+H46o6jW4giQoiR+w37tcsBQjBtjb/X3P/hkYR72CeWhYo/SVKfi0sbtWwCqRFJg/
+dAA74Zu32Oy0CXuqROBsL2lvp6NZ/OFiPIq1mZgTYE6sOh2/x7yF9N909B4Um1DK
+wcBkKSJBT3EHCsn5nnTlE79+Ha5ThcuComgtX3kpMQKBgQC+xyFq/T/rPTQ19bCg
+dLEzrwUO5ldidDcWgwpICrtpWkjdyTfOWk+LhLZbqVYhUJRfjR7NAFI3KXbxKs4t
+1nGfbD6/kXQ4jzVpmSB0Grd8gbFz+M8+ppu3gDdU8Cmx072I5NJETjM4k8lUR0i/
+EtOUVnpe6XgD2UHf+hFNvTvHjQKBgQC6vASubjQIu5fk+lomP2TJi0DBBQVNLRxA
+NctyC7H4INILFg3YcIE9Xmc+5DdZM9P3Yz0ddNfMvGPCzBhijh5acg7ttQbOxfJf
+bmsREjSbut+veLVXpsJxv9FlBif/wJOTaEPmC/jbR8a+E9d8AD9+wtgA28tPy1zH
+BOiWkVUbzwKBgQChpQXKqkPDZPtdEQyUF+ug+BWYF1FJSWD3wRYwDm/XBzmgOgL/
+5ANsGomMxsEEv0rIX0EOv8vpk0eFdhh2PDHPu567IFpSJf1CuQ0Hcy+u3hDmS5YF
+5pMf+xJm9IrPcrIwUHlciMnbkVTQBQo9Y41ELbXHv5gB4s5vIWoR7Vm+bQKBgECG
+c7+qiQRzfjs7XydURIl4Ovn3uCtA6UAu1owkCm1JOgtmpYYi6iXAl0CeL5n9VMNp
+YVbqZx9yAa7wQ4TawxZRz2LIaP/OWsArMYgGmHMvkdfaiB8vmoUf2UVlaTDw5I3N
+O5kkjeuH098ekBljJI2y2l0BljGOfhKvWYed1KD1AoGAYGDu8qf7C+dp6IKBo/qn
+AgLveHprPpY/3A9imdXUTVlcEOSxcLs1yVhlZIHEUvrqUmGisA4YIaFcNycyLKuo
+6Uu8GzhhNlNjK5AmX+BMWB3/GA/IjpughkhjeN6TOH10A2KpI36BtnMwItV41ojv
+/KCnotsISUslC8YGa4mY6rA=
+-----END PRIVATE KEY-----
+',
+  ),
+);
